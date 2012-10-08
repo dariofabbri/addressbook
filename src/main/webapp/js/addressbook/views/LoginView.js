038960e4ba90dc8717f62fdb7180dfbb4d4d54f4
Dario Fabbri <dario.fabbri@fao.org>
2012-10-08 18:40:03 +0200

    Added code for login form display.

--- src/main/webapp/js/addressbook/views/LoginView.js
+++ src/main/webapp/js/addressbook/views/LoginView.js
@@ -1,16 +1,58 @@
 var LoginView = Backbone.View.extend({
-	tagName: "div",
 	
 	template: $("#loginViewTemplate").html(),
 
-	render: function() {
-
-		//this.$el.html(_.template(this.template));
-		$(this.el).html("<h1>Pippo</h1>");
-		//return this;
+	events: {
+		"click #btnLogin" : "executeLogin",
+		"click #btnCancel" : "cancelForm",
 	},
 
-	events: {
-		//"click .delete" : "deleteBook"
+	initialize: function() {
+	
+		this.el = $("#container");
+	},
+	
+	render: function() {
+
+		$(this.el).html(_.template(this.template));
+		//$("#container").html(_.template(this.template));
+		return this;
+	},
+	
+	cancelForm: function(e) {
+		
+		e.preventDefault();
+		
+		alert("Not allowed!");
+	},
+	
+	executeLogin: function(e) {
+		
+		e.preventDefault();
+		
+		var template = $("#alertTemplate").html();
+
+		if ($("#username").val() != "admin" ||
+				$("#password").val() != "admin") {
+			$("#notification").remove();
+		    $("form>legend").after(
+		   		_.template(template,
+					{
+						alertClass: "alert-error", 
+						title: "Error", 
+						message: "Wrong credentials."}));
+		} else {
+			$("#notification").remove();
+		    $("form>legend").after(
+			   		_.template(template,
+						{
+							alertClass: "alert-success", 
+							title: "Success", 
+							message: "Login executed."}));
+		}
+
+		// Return false to cancel normal form submit event methods.
+		//
+		return false;
 	}
 });
